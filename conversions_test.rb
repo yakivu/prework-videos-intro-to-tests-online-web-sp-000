cab0fe7eff15a1811da50411acb0dbf2326c9497
Adam Walker <adamw207@yahoo.com>
2019-12-07 16:35:30 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -9,8 +9,8 @@ else
 end
 
 puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
-if answer == 28.3495
+answer = ounces_to_grams(0)
+if answer == 0.0
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
